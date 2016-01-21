6b5c30bfbfa2da657f9fea908684c9b4875a77e5
cobismith <cobi.smith@gmail.com>
2016-01-21 20:52:48 +1030

    Update recycle.js

--- recycle.js
+++ recycle.js
@@ -386,7 +386,7 @@ jQuery(document).ready(function() {
     });
 
     var actorY = point.y;
-    point = new Point(700, 500);
+    point = new Point(700, 130);
 
     var shows = _.map(showNames, function (name) {
         var show = rel.createShow(name, point.clone());
