baa54a607979548a5a25b81ae74641d15b06f623
cobismith <cobi.smith@gmail.com>
2016-01-24 16:57:50 +1030

    Update recycle.js

--- recycle.js
+++ recycle.js
@@ -1,3 +1,6 @@
+ //Note for international law mod: actor = law, while show = country (somewhat counter-intuitively)/
+
+
 /**
  * Our rel closure contains functions to show the relationships
  * between actors and the shows they're in.  It also handles the
